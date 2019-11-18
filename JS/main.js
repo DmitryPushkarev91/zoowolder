00d198ef400bd739897d43c1f08f06ac8567a936
Dmitry <pdd91dima@gmail.com>
2019-11-19 01:06:45 +0300

    update 18.11.2019

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,12 @@
 $('.carousel').slick();
+
+document.querySelector ('.navbar-wrapper').onclick = function() {
+    document.querySelector ('.navbar').classList.toggle('navbar__active');
+}
+
+let menuElem = document.getElementById('filter');
+let titleElem = menuElem.querySelector('.title');
+
+titleElem.onclick = function() {
+  menuElem.classList.toggle('open');
+};
